18a60e117507e62aba13b86979a375b2992b7262
Trey Shaffer <treyjshaffer@gmail.com>
2020-11-11 15:38:18 -0600

    Added direct to Results page on quiz complete

--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -3,20 +3,23 @@ import { Link } from 'react-router-dom';
 import Logo from '../images/white-spotify-logo.png';
 import Music1 from '../images/music-1.png';
 import Music2 from '../images/music-2.png';
+import { Container } from 'react-bootstrap';
 
 class Home extends Component {
   render() {
     return (
-        <div className="page">  
-          <img src={Logo} className="logo"alt=""/>
-          <div className="home">
-            <h2>myCollegeVibe</h2>
-            <img src={Music2} className="music2" alt=""/>
-            <Link to="/quiz" className="btn btn-to-quiz">what's my vibe?</Link>
-            <img src={Music1} className="music1" alt=""/>
-            <p>you listen to music and we tell you what university is your best fit based on your preferences</p>
-          </div>
-        </div>
+      <>
+        <Container class="page" fluid>
+            <img src={Logo} class="logo" alt="logo"/>
+            <div class="home">
+              <p>myCollegeVibe</p>
+              <img src={Music2} className="music2" alt=""/>
+              <Link to="/quiz" className="btn btn-to-quiz">what's my vibe?</Link>
+              <img src={Music1} className="music1" alt=""/>
+              <p>you listen to music and we tell you what university is your best fit based on your preferences</p>
+            </div>
+        </Container>
+      </>
     );
   }
 }
--- web/src/pages/Quiz.js
+++ web/src/pages/Quiz.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Question from '../components/Question.js';
 import Logo from '../images/white-spotify-logo.png';
 import { Navbar, Image, Container, Col } from "react-bootstrap";
+import { Redirect } from 'react-router-dom'
  
 class Quiz extends Component {
 
@@ -32,6 +33,16 @@ class Quiz extends Component {
             genre: "digital hardcore",
             playlistUri: "2fqbzU1sBBZ3gW61Q7qYhp"
           }
+        ],
+        [ // Question 3
+          {
+            genre: "alternative hip hop",
+            playlistUri: "2kHQTJJWXc54i8mX8ndYdf"
+          },
+          {
+            genre: "noise pop",
+            playlistUri: "79vSZtZGJqNVs3KYInh42V"
+          }
         ]
       ],
       questionNumber: 1,
@@ -40,36 +51,37 @@ class Quiz extends Component {
   }
 
   nextQuestion(curQuestionAnswer) {
-    const { questionAnswers, questionNumber } = this.state
+    const { questionAnswers, questionNumber, questionList } = this.state
     this.setState({
       questionAnswers: [...questionAnswers, curQuestionAnswer],
-      questionNumber: questionNumber + 1
+      questionNumber: questionNumber + 1,
     })
   }
 
   render() {
-    const { questionList, questionNumber } = this.state
-
-    let curQuestion = questionList[questionNumber-1]
-    console.log(curQuestion)
-    return (
-        <>
-          <Navbar bg="#14">
-            <Container fluid>
-              <Col xs="4" lg="8">
-                <h2><span class="navbar-text"><div class="green-text">myCollegeVibes</div></span></h2>
-              </Col>
-              <Col md="auto"/>
-              <Col xs="8" lg="4">
-                <img src={Logo} alt="logo" class="logo"i/>
-              </Col>
-            </Container>
-          </Navbar>
-          <Question 
-            choices={curQuestion}
-            onSelect={this.nextQuestion}/>
-        </>
-    );
+    const { questionList, questionNumber, questionAnswers } = this.state
+    if (questionNumber > questionList.length) {
+      return <Redirect to='/results' data={questionAnswers}/>
+    } else {
+      return (
+          <>
+            <Navbar bg="#14">
+              <Container fluid>
+                <Col xs="4" lg="8">
+                  <h2><span class="navbar-text"><div class="green-text">myCollegeVibes</div></span></h2>
+                </Col>
+                <Col md="auto"/>
+                <Col xs="8" lg="4">
+                  <img src={Logo} alt="logo" class="logo"/>
+                </Col>
+              </Container>
+            </Navbar>
+            <Question 
+              choices={questionList[questionNumber-1]}
+              onSelect={this.nextQuestion}/>
+          </>
+      );
+    }
   }
 }
 

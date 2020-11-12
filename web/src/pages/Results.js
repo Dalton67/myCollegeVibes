dbb36fe6bb13340a022fe148e2fbf648fcfe5f36
Trey Shaffer <treyjshaffer@gmail.com>
2020-11-11 20:15:48 -0600

    Got basic results page set-up

--- web/src/pages/Results.js
+++ web/src/pages/Results.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Redirect } from 'react-router-dom';
+import Logo from '../images/white-spotify-logo.png';
+import { Navbar, Image, Container, Col, Row, Tabs, Tab } from "react-bootstrap";
 import { withRouter } from "react-router";
 
 class Results extends Component {
@@ -18,9 +20,61 @@ class Results extends Component {
     if (!this.props.location.state) return <Redirect to="/" />
     else {
       return (
-          <div>
-            <h2> Results </h2>
-          </div>
+        <>
+          <Navbar bg="#14">
+            <Container fluid>
+              <Col lg="1" />
+              <Col xs="4" lg="7">
+                <p className="quiz-title-text justify-left">myCollegeVibes</p>
+              </Col>
+              <Col md="auto"/>
+              <Col xs="8" lg="4">
+                <img src={Logo} alt="logo" className="logo justify-left"/>
+              </Col>
+            </Container>
+          </Navbar>
+          <Container fluid>
+            <Row className="quiz-header">
+              <Col className="justify-center quiz-question-box">
+                <p className="quiz-question-number">Results</p>
+              </Col>
+            </Row>
+          </Container>
+          <Tabs defaultActiveKey="user-answers">
+            <Tab eventKey="user-answers" title="Your Answers">
+            <Container className="justify-center" fluid>
+              <Row className="results-header">
+                <Col className="results-header-box">
+                  <p className="results-header-text">Choice 1</p>
+                </Col>
+                <Col className="results-header-box">
+                  <p className="results-header-text">Choice 2</p>
+                </Col>
+                <Col className="results-header-box">
+                  <p className="results-header-text">Your choice</p>
+                </Col>
+              </Row>
+              {
+                this.state.data.map(question => (
+                  <Row className="results-data">
+                    <Col className="results-data-box">
+                      <p className="results-data-text">{question.choices[0].genre}</p>
+                    </Col>
+                    <Col className="results-data-box">
+                      <p className="results-data-text">{question.choices[1].genre}</p>
+                    </Col>
+                    <Col className="results-data-box">
+                      <p className="results-data-text">{question.answer}</p>
+                    </Col>
+                  </Row>
+                ))
+              }
+            </Container>
+            </Tab>
+            <Tab eventKey="universities" title="Your Universities">
+            </Tab>
+          </Tabs>
+        </>
       );
     }
   }
